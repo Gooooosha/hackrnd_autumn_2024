8095273d3f11ca1eaf94124453a4457fc3ca7745
yaonglax <stasya200411@gmail.com>
2024-10-27 00:00:52 +0300

    initial commit

--- webapp_service/frontend/src/Pages/CodeVerificationPage.jsx
+++ webapp_service/frontend/src/Pages/CodeVerificationPage.jsx
@@ -1,44 +1,11 @@
-import React, { useState } from "react";
-import Form from "../components/Form/Form";
-import Input from "../components/Input/Input";
-import Button from "../components/Button/Button";
-import "./styles/Code.css";
+import React from "react";
+import CodeVerification from "../components/CodeVerification";
 
 function CodeVerificationPage() {
-  const [code, setCode] = useState("");
-  const isValid = /^\d{4}$/.test(code);
-
-  const validateCode = () => {
-    if (isValid) {
-      alert("Код подтвержден");
-    } else {
-      alert("Код должен содержать 4 цифры");
-    }
-  };
-
-  const handleInputChange = (event) => {
-    const inputValue = event.target.value;
-    if (inputValue.match(/^\d*$/)) {
-      setCode(inputValue);
-    }
-  };
+ 
 
   return (
-    <form className="form">
-      <h1>Подтверждение входа</h1>
-      <div className="form__container">
-        <Input
-          type="text"
-          className="form__input input"
-          placeholder="Код подтверждения"
-          value={code}
-          onChange={handleInputChange} 
-        />
-        <Button type="button" onClick={validateCode}>
-          Подтвердить
-        </Button>
-      </div>
-    </form>
+   <CodeVerification></CodeVerification>
   );
 }
 
